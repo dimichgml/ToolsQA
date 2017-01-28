06be32eb210b398c2f3ecce130dbfcd6aaac4164
Dmitriy Sergeev <dimichgml@gmail.com>
2017-01-28 17:26:04 +0700

    To create first test

---
+++ ToolsQA/NUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+
+namespace ToolsQA
+{
+    [TestFixture]
+    public class NUnitTest
+    {
+        private IWebDriver _driver;
+              
+        [SetUp]
+        public void Initialize()
+        {
+            _driver = new ChromeDriver();          
+        }
+        [Test]
+        public void TestApp()
+        {
+            _driver.Url = "http://www.demoqa.com";
+
+        }
+        [TearDown]
+        public void EndTest()
+        {
+            _driver.Quit();
+            _driver = null;
+        }
+    }
+}

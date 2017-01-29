e6dd488126e02cf5184bb00878c7d2c36cf90c73
Dmitriy Sergeev <dimichgml@gmail.com>
2017-01-29 21:24:25 +0700

    Revert "Test for login form Litecart"
    This reverts commit f986c449b0c3058e470af56c9ad8f587dd5c16fe.

--- ToolsQA/NUnitTest.cs
+++ ToolsQA/NUnitTest.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
 using NUnit.Framework;
 
 
@@ -11,23 +11,17 @@ namespace ToolsQA
     public class NUnitTest
     {
         private IWebDriver _driver;
-        private WebDriverWait _wait;
-
+              
         [SetUp]
         public void Initialize()
         {
-            _driver = new ChromeDriver();
-            _driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
-            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));     
+            _driver = new ChromeDriver();          
         }
         [Test]
         public void TestApp()
         {
-            _driver.Url = "http://localhost/litecart/admin/";
-            _driver.FindElement(By.Name("username")).SendKeys("admin");          
-            _driver.FindElement(By.Name("password")).SendKeys("admin");            
-            _driver.FindElement(By.Name("remember_me")).Click();           
-            _driver.FindElement(By.Name("login")).Click();            
+            _driver.Url = "http://www.demoqa.com";
+
         }
         [TearDown]
         public void EndTest()

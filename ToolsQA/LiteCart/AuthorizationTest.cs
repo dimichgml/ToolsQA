4c1a0ec4712b2cd37438e6d217af11268fae76ee
Dmitriy Sergeev <dimichgml@gmail.com>
2017-02-07 13:10:28 +0700

    MenuClick

--- ToolsQA/LiteCart/AuthorizationTest.cs
+++ ToolsQA/LiteCart/AuthorizationTest.cs
@@ -3,6 +3,8 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ToolsQA.LiteCart
 {
@@ -16,11 +18,11 @@ namespace ToolsQA.LiteCart
         public void Initialize()
         {
             _driver = new ChromeDriver();
-            _driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
-            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            _driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(3));
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
         }
         [Test]
-        public void TestApp()
+        public void TestAuthorization()
         {
             _driver.Url = "http://localhost/litecart/admin/";
             _driver.FindElement(By.Name("username")).SendKeys("admin");
@@ -28,11 +30,54 @@ namespace ToolsQA.LiteCart
             _driver.FindElement(By.Name("remember_me")).Click();
             _driver.FindElement(By.Name("login")).Click();
         }
+        [Test]
+        public void NemuClick()
+        {
+            TestAuthorization();
+            int count = _driver.FindElements(By.CssSelector("#app- > a")).Count;
+            List<IWebElement> _menu;
+            for (int i = 0; i < count; i++)
+            {
+                _menu = _driver.FindElements(By.CssSelector("#app- > a")).ToList();
+                _menu[i].Click();                                
+                Assert.False(!IsElementExists(By.TagName("h1")));
+                if (IsElementExists(By.CssSelector("#app- > ul > li")))
+                {
+                    int subCount = _driver.FindElements(By.CssSelector("#app- > ul > li")).Count;
+                    if (subCount > 0)
+                    {
+                        List<IWebElement> submenu;
+                        for (int j = 0; j < subCount; j++)
+                        {
+                            submenu = _driver.FindElements(By.CssSelector("#app- > ul > li")).ToList();
+                            submenu[j].Click();                            
+                            Assert.False(!IsElementExists(By.TagName("h1")));
+                        }
+                    }
+
+                }                            
+            }
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+
+        }
         [TearDown]
         public void EndTest()
         {
             _driver.Quit();
             _driver = null;
+        }
+
+        public bool IsElementExists(By iCssSelector)
+        {
+            try
+            {
+                _driver.FindElement(iCssSelector);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
         }
     }
 }
